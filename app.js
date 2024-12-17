70d0152f9bfe4cec62454c06a5a430759eecced2
ThalaVR <123vimalvr@gmail.com>
2024-12-17 16:02:24 +0530

    set json

--- app.js
+++ app.js
@@ -2,20 +2,36 @@
 
 const diceBtn = document.querySelector(".button-section");
 const adviceNumber = document.querySelector(".advice-number");
+const kural_line_1 = document.querySelector(".line-1");
+const kural_line_2 = document.querySelector(".line-2");
 const adviceText = document.querySelector(".quotes");
-const apiUrl = "https://api.adviceslip.com/advice";
+// const apiUrl = "https://api.adviceslip.com/advice";
+
+const apiJson = "data/thirukkural.json"
 
 //Button click event
 diceBtn.addEventListener("click", () => {
+  let clickNo = 0;
   //Request Data
-  fetch(apiUrl, { cache: "no-cache" })
-    .then((response) => response.json())
+  fetch(apiJson)
+  .then((response) => {
+    if (!response.ok) {
+      throw new Error(`HTTP error! status: ${response.status}`);
+    }
+    return response.json(); // Parse the JSON data
+  })
     .then((response) => {
-      let data = response.slip;
-      let dataId = data.id;
-      let dataAdvice = data.advice;
-      //Inject to DOM
-      adviceNumber.innerHTML = `advice # ${dataId}`;
-      adviceText.innerHTML = dataAdvice;
+      const kural = response.kural[0]; 
+      // Extract data
+      const kuralNumber = kural.Number;
+      const kuralLine1 = kural.Line1;
+      const kuralLine2 = kural.Line2;
+      const kuralTranslation = kural.mv;
+
+      // Inject to DOM
+      adviceNumber.innerHTML = `திருக்குறள் # ${kuralNumber}`;
+      kural_line_1.innerHTML = `${kuralLine1}`
+      kural_line_2.innerHTML = `${kuralLine2}`
+      adviceText.innerHTML = ` ${kuralTranslation}`
     }).catch(error => console.error(error));
 });
